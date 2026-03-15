7486abaeb65aeb680c4d1829a4e6ec979075a4ca
ManuelSMC <152463014+ManuelSMC@users.noreply.github.com>
2026-03-15 15:35:13 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const { buildReinscripcionChain } = require("./chain/buildReinscripcionChain");
 
 const app = express();
 const port = process.env.PORT || 3000;
-//
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
